d858429895979f89e6a54e6717b0aa2efcd6f13e
eapepe0 <eapepe0@gmail.com>
2021-11-16 03:36:39 -0300

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <img
+        src="../images/react-icon-small.png"
+        alt="logo"
+        className="nav--logo"
+      />
+      <h3 className="nav--logo_text">React Facts</h3>
+      <h4 className="nav--title">React Course - Project 1 </h4>
+    </nav>
+  );
+}
